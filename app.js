8422afe5924d1235e4fdc723c5ef23cc2f98a0ea
chlkrisz <39501471+chlkrisz@users.noreply.github.com>
2018-09-15 19:14:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,26 +4,18 @@ const config = require("./config.json");
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-  client.user.setActivity(`/help`);
+  client.user.setActivity(`CS:GO`);
 });
 
 client.on("guildCreate", guild => {
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`CS:GO`);
 });
 
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`CS:GO`);
 });
-
-function altgen() {
-  var rand = ['user:155',
-  'asgasg:123',
-  '15132:sfasf'];
-
-  return rand[Math.floor(Math.random()*rand.length)];
-}
 
 client.on("message", async message => {
 
@@ -42,6 +34,11 @@ if(command === "yt szoulohun") {
 }
 if(command === "yt chlkrisz") {
   message.channel.send("**Chlkrisz YT:** https://www.youtube.com/channel/UCb82WRTuK2X0xllzSVrZPHQ")  
+}
+if(command === "help") {
+  message.channel.send("Parancsok:")
+  message.channel.send("**/help** - Ez a panel")
+  message.channel.send("**/yt** - Lekérdezheted a főbb tagok YT csatornájának linkjét.")
 }
 });
 
