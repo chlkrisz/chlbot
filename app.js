7e13a2940be9e5a98b2c53eb7df846bd1fac9aa7
chlkrisz <39501471+chlkrisz@users.noreply.github.com>
2018-09-15 22:15:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,25 +8,25 @@ client.on("ready", () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'baszd') {
+  if (msg.contains === 'baszd') {
     msg.reply('Naa! Azért ne káromkodd ide a Dunát!');
   }
 });
 
 client.on('message', msg => {
-  if (msg.content === 'kurva') {
+  if (msg.contains === 'kurva') {
     msg.reply('Naa! Azért ne káromkodd ide a Dunát!');
   }
 });
 
 client.on('message', msg => {
-  if (msg.content === 'geci') {
+  if (msg.contains === 'geci') {
     msg.reply('Naa! Azért ne káromkodd ide a Dunát!');
   }
 });
 
 client.on('message', msg => {
-  if (msg.content === 'fasz') {
+  if (msg.contains === 'fasz') {
     msg.reply('Naa! Azért ne káromkodd ide a Dunát!');
   }
 });
@@ -42,48 +42,7 @@ client.on("guildCreate", guild => {
   client.user.setActivity(` 000000111011000111100001101001000101000000  00001  00000  000000000000`);
 });
 
-client.on('message', msg => {
-  if (msg.content.startsWith(config.prefix+'join')) {
-    let [command, ...channelName] = msg.content.split("_");
-    if (!msg.guild) {
-      return msg.reply('no private service is available in your area at the moment. Please contact a service representative for more details.');
-    }
-    const voiceChannel = msg.guild.channels.find("name", channelName.join(" "));
-    //console.log(voiceChannel.id);
-    if (!voiceChannel || voiceChannel.type !== 'voice') {
-      return msg.reply(`I couldn't find the channel ${channelName}. Can you spell?`);
-    }
-    voiceChannel.join()
-      .then(conn => {
-        msg.reply('ready!');
-        // create our voice receiver
-        const receiver = conn.createReceiver();
 
-        conn.on('speaking', (user, speaking) => {
-          if (speaking) {
-            msg.channel.sendMessage(`I'm listening to ${user}`);
-            // this creates a 16-bit signed PCM, stereo 48KHz PCM stream.
-            const audioStream = receiver.createPCMStream(user);
-            // create an output stream so we can dump our data in a file
-            const outputStream = generateOutputFile(voiceChannel, user);
-            // pipe our audio data into the file stream
-            audioStream.pipe(outputStream);
-            outputStream.on("data", console.log);
-            // when the stream ends (the user stopped talking) tell the user
-            audioStream.on('end', () => {
-              msg.channel.sendMessage(`I'm no longer listening to ${user}`);
-            });
-          }
-        });
-      })
-      .catch(console.log);
-  }
-  if(msg.content.startsWith(config.prefix+'leave')) {
-    let [command, ...channelName] = msg.content.split(" ");
-    let voiceChannel = msg.guild.channels.find("name", channelName.join(" "));
-    voiceChannel.leave();
-  }
-});
 
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
