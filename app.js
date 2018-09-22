85a648d014b1cf4463918a95ba107d021e79c8ba
chlkrisz <39501471+chlkrisz@users.noreply.github.com>
2018-09-22 16:42:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const config = require("./config.json");
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-  client.user.setActivity("SzouloHUN's live", {
-    type: "WATCHING",
-  });
-   client.user.setActivity("dnd");
+   client.user.setActivity("dnd", "in development hell");
 });
 
 
