ea9662454d3db8ba324fddbd284b1feed19c77b5
chlkrisz <39501471+chlkrisz@users.noreply.github.com>
2018-09-23 20:13:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const config = require("./config.json");
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-  cleint.user.setPresence('Iratkozz fel');
+  client.user.setPresence('Iratkozz fel');
 
 
 client.on('message', msg => {
