a733b6bcc09e3f1811a5b93ebbfb3ab448557b35
hungaryck <39501471+hungaryck@users.noreply.github.com>
2018-10-27 14:55:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on("ready", () => {
+  client.user.setActivity('/help', {type: "WATCHING"});
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
 });
 
