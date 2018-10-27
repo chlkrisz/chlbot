6793c391ade1fe65a45a6aca9e190e19c09be5b5
hungaryck <39501471+hungaryck@users.noreply.github.com>
2018-10-27 15:00:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on("ready", () => {
-  client.user.setActivity('/help', {type: "STREAMING"});
+  client.user.setActivity('/help', {type: "STREAMING", url: "https://www.twitch.tv/mrmuffinkappa"});
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
 });
 
